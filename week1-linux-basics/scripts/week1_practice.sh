64937a00bf467852012716801ac7bbcee5ef9fad
Anjitha Mangalanandan <anjitham883@gmail.com>
2026-02-28 15:36:03 +0530

    Finalize Week 1 reproducible directory structure and automation script

---
+++ week1-linux-basics/scripts/week1_practice.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+set -e
+
+# Create directories
+mkdir -p data raw_data processed_data results notes
+
+# Create dummy FASTQ files
+touch raw_data/sample1.fastq raw_data/sample2.fastq
+
+# Create working copies from raw sequencing data
+cp raw_data/sample1.fastq data/
+cp raw_data/sample2.fastq processed_data/
+
+# Rename file
+mv data/sample1.fastq data/sample1_raw.fastq
+
+# Create and delete temp file
+touch temp_file.txt
+rm temp_file.txt
+
+echo "Week 1 practice complete."
